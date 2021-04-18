7fe1e9e5e0c5bb1462b17f0ecdaef8623e420af4
Ashquinn Gordon <ashquinngordon@gmail.com>
2021-04-18 11:18:56 -0400

    Id's added to search conatainer and weather card container. Also variables added to js for weather cards, search button, and city search value.

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 // API Key: 7afb4c5ab853916fad78aa45143c0ef7
+let citySearch = document.getElementById('cityInput')
+const mainWeatherContainer = document.getElementById('mainWeatherDisplay')
+const weatherCards = document.getElementById('miniWeatherCard')
+const searchBtn = document.getElementById('searchBtn')
 
-function getWeatherData(city){
-    fetchAPI = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=7afb4c5ab853916fad78aa45143c0ef7`
-    fetch(fetchAPI)
+
+function getWeatherData(){
+    let cityName = citySearch.value
+   
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=7afb4c5ab853916fad78aa45143c0ef7`)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
-        console.log(data)
+        console.log(data.temp)
     })
 }
 
-getWeatherData('Atlanta')
+searchBtn.addEventListener("click", getWeatherData);
