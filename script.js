ec94ef22bad1e3d7a85bd32201302b2165823c68
Ashquinn Gordon <ashquinngordon@gmail.com>
2021-04-28 14:41:50 -0400

    comments added to throughout the js file to explain functionality.

--- script.js
+++ script.js
@@ -4,29 +4,34 @@ const todayContainer = document.getElementById('mainWeatherDisplay')
 const searchBtn = document.getElementById('searchBtn')
 const cardDeck = document.getElementById('cardDeck')
 
+//This function calls the weather API and renders the current forecast along with the five day forcast
 function getWeatherData(e){
     e.preventDefault()
     let cityName = citySearch.value
     saveCity(cityName)
     
+    //API call too openweathermap database to retrieve data to render for the forecast.
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=7afb4c5ab853916fad78aa45143c0ef7`)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
-        console.log(data)
+        //Creates elements to append todays weather.
+        //console.log(data)
         let currentCity = document.createElement('h1');
         let todayTemp = document.createElement('p');
         let todayHumidity = document.createElement('p');
         let todayWind = document.createElement('p');
         let weatherIcon = document.createElement('img')
 
+        //Setting the text content and weather icon for todays weather/.
         currentCity.textContent = `${cityName} (${moment(data.list[0].dt_txt).format('MMM Do, YYYY')})`;
         todayTemp.textContent = `Temp: ${data.list[0].main.temp} ˚F`
         todayHumidity.textContent = `Humidity: ${data.list[0].main.humidity}%`
         todayWind.textContent = `Wind speed: ${data.list[0].wind.speed} MPH`
         weatherIcon.setAttribute('src', "https://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png")
 
+        //Appending all text content to the 'todayContainer'. This will display the current weather to the application.
         todayContainer.textContent = '';
         todayContainer.appendChild(currentCity);
         todayContainer.appendChild(todayTemp);
@@ -36,10 +41,10 @@ function getWeatherData(e){
 
         cardDeck.innerHTML = '';
 
-
+        //The for loop will iterate through the data retrieved from the API call. 'i' is defined as 3 so that we are given the weather when the time is 12:00PM, the API returns data in 3 hour increments so if i is 3 then we will be retrieving the 4th index of the weather array which is the weather for 12:00PM.
         for (let i = 3; i <= data.list.length ; i+=8) {
         
-        
+        //The following lines of code are used to create the elements for each weather card and set the text content/attribute for each card. 
         let weatherCards = document.createElement('div')
         let cardBody = document.createElement('div')
         let forecastIcon = document.createElement('img')
@@ -56,6 +61,7 @@ function getWeatherData(e){
         humidity.textContent = `Humidity: ${data.list[i].main.humidity}%`
         weatherIcon.textContent = data.list[i].main.temp;
 
+        //Appends text content to the card bdy which will hold the data for the next five days. 
         cardBody.appendChild(date);
         cardBody.appendChild(forecastIcon);
         cardBody.appendChild(currentTemp);
@@ -63,19 +69,23 @@ function getWeatherData(e){
         cardBody.appendChild(humidity);
         cardBody.setAttribute('class', 'card-body text-white bg-primary rounded');
 
+        //Appends the card body the the weather cards container.
         weatherCards.appendChild(cardBody);
         weatherCards.setAttribute('class', 'card');
 
+        //Appends the weather cards to the cardDeck.
         cardDeck.appendChild(weatherCards)
         console.log(data)
         }
     })
 }
+//This function will save each searched city to local storage. 
 function saveCity(newCity) {
     let savedCities = JSON.parse(localStorage.getItem('savedCities')) || []
     savedCities.push(newCity)
     localStorage.setItem('savedCities', JSON.stringify(savedCities))
 }
+//This function will display the cities from local storage onto the console. This verifies that local storage has received the data.
 function displaySearch() {
     let savedCities = JSON.parse(localStorage.getItem('savedCities')) || []
     for (let i = 0; i < savedCities.length; i++){
